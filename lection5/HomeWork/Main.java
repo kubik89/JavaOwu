ff6fe404fe448190c4409c499c0233db628583a6
Volodymyr <kubik89@yahoo.com>
2020-12-10 16:19:16 +0200

    DZ 5 - 1 task in progress

--- lection5/HomeWork/Main.java
+++ lection5/HomeWork/Main.java
@@ -1,50 +1,61 @@
 package lection5.HomeWork;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-//        store1[0].addToShop("Огірок", 25);
-//        store1[1].addToShop("Помідор", 35);
-//        System.out.println(store1);
-        List<Product> list1 = new ArrayList<>();
-
-//        Product product1 = new Product("Огірок0", 25, ProductType.VEGETABLES);
-//        list1.add(product1.someFunction("Огірок1", 25, ProductType.VEGETABLES));
-//        list1.add(product1.someFunction("Огірок2", 30, ProductType.VEGETABLES));
-//        list1.add(product1.someFunction("Огірок3", 35, ProductType.VEGETABLES));
-//        System.out.println(list1);
-
         List<Store> storeList = new ArrayList<>();
+        Store store1 = new Store();
+        storeList.add(store1.addToShopNew("Огірок0", 25, ProductType.VEGETABLES));
+        storeList.add(store1.addToShopNew("Огірок1", -25, ProductType.VEGETABLES));
+        storeList.add(store1.addToShopNew("Огірок2", 55, ProductType.VEGETABLES));
+        storeList.add(store1.addToShopNew("Горілка", 155, ProductType.ALCOHOL));
+        storeList.add(store1.addToShopNew("Пиво", 45, ProductType.ALCOHOL));
+        storeList.add(store1.addToShopNew("Коньяк", 450, ProductType.ALCOHOL));
+        storeList.add(store1.addToShopNew("Авокадо", 100, ProductType.FRUICTS));
+        storeList.add(store1.addToShopNew("Фінік", -5, ProductType.FRUICTS));
+        storeList.add(store1.addToShopNew("Бан1ан", 55, ProductType.FRUICTS));
+        storeList.add(store1.addToShopNew("Цукерки", -120, ProductType.SWEETS));
+        storeList.add(store1.addToShopNew("Печиво", 70, ProductType.SWEETS));
+        storeList.add(store1.addToShopNew("Зефір", 90, ProductType.SWEETS));
 
-        storeList.add(new Store().addToShopNew("Огірок0", 25, ProductType.VEGETABLES));
-        storeList.add(new Store().addToShopNew("Огірок1", -25, ProductType.VEGETABLES));
-        storeList.add(new Store().addToShopNew("Огірок2", 55, ProductType.VEGETABLES));
-        storeList.add(new Store().addToShopNew("Горілка", 155, ProductType.ALCOHOL));
-        storeList.add(new Store().addToShopNew("Пиво", 45, ProductType.ALCOHOL));
-        storeList.add(new Store().addToShopNew("Коньяк", 450, ProductType.ALCOHOL));
-        storeList.add(new Store().addToShopNew("Авокадо", 100, ProductType.FRUICTS));
-        storeList.add(new Store().addToShopNew("Фінік", -5, ProductType.FRUICTS));
-        storeList.add(new Store().addToShopNew("Банан", 55, ProductType.FRUICTS));
-        storeList.add(new Store().addToShopNew("Цукерки", -120, ProductType.SWEETS));
-        storeList.add(new Store().addToShopNew("Печиво", 70, ProductType.SWEETS));
-        storeList.add(new Store().addToShopNew("Зефір", 90, ProductType.SWEETS));
+        storeList.remove(store1.getIntByPrice(70)); // ????
 
-        for (Store product : storeList) {
-            System.out.println(product);
-        }
 
-//        System.out.println(list1.get(1));
-//        store1.setList(new Product("Томат", 60, ProductType.VEGETABLES));
+
+
+
+
+
+//        storeList.remove(5);
+
+//        store1.deleteFromShopByName("Печиво");
+
+//        for (Store product : storeList) {
+//            System.out.println(product);
+//        }
+
+//        for (int i = 0; i < storeList.size(); i++) {
+//            System.out.println(storeList.);
+//            storeList.remove(5);
+//        }
+//        store1.getIntByName("Зефір");
+
+//        System.out.println(storeList.remove(5));
+//        System.out.println(store1.getList().getName().length());
+
+
+//        store1.deleteFromShopByName("Фінік");
+
+//        System.out.println(storeList.size());
 //        System.out.println(store1.getList());
 
-//        System.out.println(store1.getList());
 
 
-//        List<Store> list1 = new ArrayList<>();
-//        list1.add(new Store(new Product("Помідор", 45, ProductType.VEGETABLES)));
 
     }
 }
--- lection5/HomeWork/Store.java
+++ lection5/HomeWork/Store.java
@@ -9,12 +9,31 @@ public class Store {
 
     public void setList(Product list) {
         this.list = list;
-        }
+    }
 
-    public Product addToShopNew (String name, int price, ProductType type) {
-        if (price>=0) {
+    public Product addToShopNew(String name, int price, ProductType type) {
+        if (price > 0) {
+            for (int j = 0; j <= 9; j++) {
+                String number = Integer.toString(j);
+                if (name.contains(number)) {
+                    return null;
+                }
+            }
             return new Product(name, price, type);
-        } return null;
+        } else return null;
+    }
+
+    public void deleteFromShopByName(String name) {
+        if (getList().getName().equals(name)) {
+            System.out.println("Знайдено" + getList().getName());
+        }
+//        return getList().getName().indexOf(name);
+    }
+    public int getIntByPrice (int price) {
+        if (getList().getPrice() == price) {
+            return getList().getName().length();
+        }
+        return getList().getName().length();
     }
 
     @Override
