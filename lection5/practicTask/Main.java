3bf32b397a09ed0dd3b9c11db1ac74e95022e3f5
Volodymyr <kubik89@yahoo.com>
2020-12-11 17:12:51 +0200

    Practic task 5 -

--- lection5/practicTask/Main.java
+++ lection5/practicTask/Main.java
@@ -7,39 +7,65 @@ public class Main {
         Pet pet1 = new Pet("Dog1", 5);
         Pet pet2 = new Pet("Cat1", 2);
 
-//        List<Pet> pets = new ArrayList<>();
-//        pets.add(pet1);
-//        pets.add(pet2);
-
-//        System.out.println(pets);
-
         Person p1 = new Person(1, "Person1", 25);
         Person p2 = new Person(2, "Person2", 40);
+        Person p3 = new Person(3, "Person3", 50);
 
         Map<Person, List<Pet>> map = new HashMap<>();
         map.put(p1, new ArrayList<>());
         map.put(p2, new ArrayList<>());
 
+        addParticipantInClub(p3, map);
+        addAnimalForParticipant(1, pet1, map);
+        addAnimalForParticipant(2, pet2, map);
+        removeAnimalFromParticipant(2, map);
+
+        deleteParticipantFromClub(1, map);
+
+        printMap(map);
+
         ZooClub zooClub1 = new ZooClub(map);
 
-//        zooClub1.setClub(map);
-
 //        System.out.println(zooClub1.getClub());
-        addParticipantInClub(1, pet1, map);
-        addParticipantInClub(2, pet2, map);
-        printMap(map);
     }
 
-    public static void addParticipantInClub(int personId, Pet pet, Map<Person, List<Pet>> map) {
+    public static void addAnimalForParticipant(int personId, Pet pet, Map<Person, List<Pet>> map) {
         Set<Person> set = map.keySet(); // пробігаюсь по ключах мапи
         for (Person newSet : set) {
             if (newSet.getId() == personId) {
-//                System.out.println(newSet.getName());
                 map.get(newSet).add(pet);
-//                System.out.println(map);
             }
         }
     }
+
+    public static void addParticipantInClub(Person person, Map<Person, List<Pet>> map) {
+        map.put(new Person(person.getId(), person.name, person.getAge()), new ArrayList<>());
+    }
+
+    public static void removeAnimalFromParticipant(int personId, Map<Person, List<Pet>> map) {
+        Set<Person> people = map.keySet();
+        for (Person person : people) {
+//            System.out.println(person);
+            if (person.getId() == personId) {
+//                System.out.println(map.get(person)); // Отримую тварину, тут person є ключем, а метод шукає по ключу
+                for (int i = 0; i < map.get(person).size(); i++) {
+                    map.get(person).clear(); // видаляю обєкт який міститься в ключі Person, а це тварина
+                }
+            }
+        }
+    }
+
+    public static void deleteParticipantFromClub(int personId, Map<Person, List<Pet>> map) {
+        Set<Person> people = map.keySet();
+        for (Person person : people) {
+            if (person.getId() == personId) {
+                System.out.println("Знайдено - " + person);
+
+            }
+        }
+    }
+//        }
+//    }
 
     public static void printMap(Map<Person, List<Pet>> map) {
         Set<Map.Entry<Person, List<Pet>>> entries = map.entrySet();
@@ -47,9 +73,9 @@ public class Main {
             System.out.println(entry.getKey() + ": ");
             for (Pet pet : entry.getValue()) {
                 System.out.println("\t" + pet);
-
             }
         }
     }
+
 }
 
